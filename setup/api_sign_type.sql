c3c299595a1876a53024671fe10711ec3e25662e
Peter Serwylo <peter@ivt.com.au>
2014-08-28 19:05:35 +0930

    More work on models, initial sql import file.

---
+++ setup/api_sign_type.sql
@@ -0,0 +1,5 @@
+INSERT INTO api_sign_type ( code, label ) VALUES
+	( 'P',  'Parking' ),
+	( 'LZ', 'Loading Zone' ),
+	( 'N',  'No Parking' ),
+	( 'U',  'Unknown' );
--- src/api/models.py
+++ src/api/models.py
@@ -1,14 +1,47 @@
 from django.contrib.gis.db import models
 
 
+class SignType(models.Model):
+    code = models.CharField(max_length=2)
+    label = models.CharField(max_length=20)
+
+
+class SignArchetype(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    start_day = models.IntegerField()
+    end_day = models.IntegerField()
+
+    permit = models.BooleanField()
+    requires_pay_ticket = models.BooleanField()
+    requires_pay_meter = models.BooleanField()
+    requires_disability_permit = models.BooleanField()
+
+    duration_mins = models.IntegerField()
+
+    type = models.ForeignKey(SignType)
+
+    def requires_pay(self):
+        return self.requires_pay_meter or self.requires_pay_ticket
+
+
 class ParkingBay(models.Model):
     street_marker = models.CharField(max_length=10)
     geom = models.PointField()
+
+    sign_archetypes = models.ManyToManyField(SignArchetype, through='ParkingBaySignArchetypeRelationship')
 
     objects = models.GeoManager()
 
     def __str__(self):
         return self.street_marker
+
+
+class ParkingBaySignArchetypeRelationship(models.Model):
+    parking_bay = models.ForeignKey(ParkingBay)
+    sign_archetype = models.ForeignKey(SignArchetype)
+    last_seen = models.DateTimeField()
 
 
 class ParkingEvent(models.Model):

5965759bceb1af877469b32bcfcb349ab3a9efef
Lyndon D'Arcy <lyndon.darcy@gmail.com>
2014-08-29 09:50:36 +1000

    Added day of week ref table

--- setup/api_day_of_week.sql
+++ setup/api_day_of_week.sql
@@ -1,1 +1,8 @@
-INSERT 
+﻿INSERT INTO api_dayofweek ( id, day_number, short_name, name ) VALUES
+(0, 0, 'Mon', 'Monday'),
+(1, 1, 'Tue', 'Tuesday'),
+(2, 2, 'Wed', 'Wednesday'),
+(3, 3, 'Thu', 'Thursday'),
+(4, 4, 'Fri', 'Friday'),
+(5, 5, 'Sat', 'Saturday'),
+(6, 6, 'Sun', 'Sunday');

8d53f3405b20d0b0b78e7447a0d6d03225e01d0f
Lyndon D'Arcy <lyndon.darcy@gmail.com>
2014-08-29 13:38:19 +1000

    Added SignArchetype serialization and improved ParkingBay serialization.

--- src/api/views.py
+++ src/api/views.py
@@ -4,8 +4,9 @@ from rest_framework.response import Response
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
-from api.models import ParkingBay
-from api.serializers import ParkingBaySerializer
+from api.models import ParkingBay, SignArchetype, ParkingBaySignArchetypeRelationship
+from api.serializers import (ParkingBaySerializer, SignArchetypeSerializer,
+ ParkingBaySignArchetypeRelationshipSerializer)
 
 
 class JSONResponse(HttpResponse):
@@ -21,6 +22,24 @@ def bay_list(request):
     if request.method == 'GET':
         bays = ParkingBay.objects.all()[:100]
         serializer = ParkingBaySerializer(bays, many=True)
+        return Response(serializer.data)
+
+@api_view(['GET'])
+def sign_archetype_list(request):
+    if request.method == 'GET':
+        signs = SignArchetype.objects.all()
+        serializer = SignArchetypeSerializer(signs, many=True)
+        return Response(serializer.data)
+
+@api_view(['GET'])
+def parkingbaysignarchetyperelationship_detail(request, pk):
+    try:
+        pbsar = ParkingBaySignArchetypeRelationship.objects.get(id=pk)
+    except ParkingBaySignArchetypeRelationship.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ParkingBaySignArchetypeRelationshipSerializer(pbsar)
         return Response(serializer.data)
 
 @api_view(['GET'])
--- src/parku/urls.py
+++ src/parku/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from api.views import bay_list, bay_detail
+from api.views import bay_list, bay_detail, sign_archetype_list, parkingbaysignarchetyperelationship_detail
 from web import views
 
 
@@ -10,7 +10,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/bays$', bay_list),
-    url(r'^api/bays/(?P<pk>[0-9]+)/$', bay_detail),
+    url(r'^api/sign_archetypes$', sign_archetype_list),
+    url(r'^api/bays/(?P<pk>[0-9]+)/$', bay_detail, name='bay-detail'),
+    url(r'^api/parkingbaysignarchetyperelationships/(?P<pk>[0-9]+)/$', 
+    	parkingbaysignarchetyperelationship_detail,
+    	name='parkingbaysignarchetyperelationship-detail'),
     url(r'^$', views.index, name='home'),
 )
 
